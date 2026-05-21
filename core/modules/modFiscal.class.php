1bd892880c8ba48c91ed2405be04cfcc1a705082
IagoMelo01 <iagomello160@gmail.com>
2026-05-21 11:52:15 -0300

    version min

--- core/modules/modFiscal.class.php
+++ core/modules/modFiscal.class.php
@@ -156,7 +156,7 @@ class modFiscal extends DolibarrModules
 		// Prerequisites
 		$this->phpmin = array(7, 2); // Minimum version of PHP required by module
 		// $this->phpmax = array(8, 0); // Maximum version of PHP required by module
-		$this->need_dolibarr_version = array(23, -3); // Minimum version of Dolibarr required by module
+		$this->need_dolibarr_version = array(21, -0); // Minimum version of Dolibarr required by module
 		// $this->max_dolibarr_version = array(19, -3); // Maximum version of Dolibarr required by module
 		$this->need_javascript_ajax = 0;
 
